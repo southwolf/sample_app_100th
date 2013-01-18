775dddcb1340ed387a3adf205a0e430b75a8f817
SouthWolf <wp.southwolf@gmail.com>
2013-01-18 10:39:50 +0000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '6afc966d981c101f3265774fbac0dde29187c5eaa7c38e1791483a64fce326ba85188ecdc3f8d117df333fd8294bc45e734adc66358f1a412d0603b3c26f7741'
